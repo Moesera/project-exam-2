f39caf291475cf579dac7f1ea6f6006bc2af3281
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-14 16:42:06 +0200

    icons import an styling

--- src/layout/Nav/index.jsx
+++ src/layout/Nav/index.jsx
@@ -1,23 +1,35 @@
 import { Link } from "react-router-dom";
 
+import SearchIcon from "../../assets/interface/icons8-search-30.png";
+import FilterIcon from "../../assets/interface/icons8-tune-50.png";
+
 function Nav() {
   return (
-    <nav className="flex">
-      <container>
-        <input type="search" placeholder="Search venues" />
-        <label></label>
+    <>
+      <container className="flex items-center justify-center w-5/7">
+        <input className="h-16.5 px-16 border rounded-full border-stone-grey w-full" type="text" placeholder="Search venues" />
+        <label className="absolute flex items-center justify-between pl-1 pr-1 w-5/7">
+          <div>
+            <img className="w-10" src={SearchIcon} alt="search" />
+          </div>
+          <div className="p-2 border rounded-full">
+            <img className="w-9" src={FilterIcon} alt="filter" />
+          </div>
+        </label>
       </container>
-      <ul className="flex">
-        <li>
-          <img src="#" alt="bookings img" />
-          bookings
+      <nav className="flex self-center justify-center">
+        <ul className="fixed inset-x-0 bottom-0 bg-slate-400">
+          <li>
+            <img src="#" alt="bookings img" />
+            bookings
           </li>
-        <li>
-          <img src="#" alt="profile img" />
-          <Link>profile</Link>
-        </li>
-      </ul>
-    </nav>
+          <li>
+            <img src="#" alt="profile img" />
+            <Link>profile</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
