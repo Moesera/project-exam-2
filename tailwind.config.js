dd1c2c3036d9225c997faa16e92aeb04f3b0df92
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-14 16:42:47 +0200

    custom - colors and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    color: {
+   "pale-white": "#FDFDFD",
+   "gray": "#CBCBCB",
+   "stone-gray": "#D8D8D8",
+   "green": "#94EF69",
+   "red": "#CD3C3C",
+   "blue": "#1C6ECD"
+
+    },
+    extend: {
+      spacing: {
+        "16.5": "4.5rem",
+        "3/7": "75%",
+        "4/7": "85%",
+        "5/7": "95%",
+        "11/12": "90%"
+      },
+    },
   },
   plugins: [],
 };
