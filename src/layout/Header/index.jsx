e073918f464eea6c8defca59dafd4aeda83a91f5
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-14 16:41:54 +0200

    logo import

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,9 +1,13 @@
 import Nav from "../Nav";
 
+import Logo from "../../assets/images/logo/icons8-floating-island-beach-100.png";
+
 function Header() {
   return (
-    <header className="flex pr-4 pl-4">
-      <img src="#" alt="logo" />
+    <header className="flex justify-center h-40 border bg-pale-white">
+      <div className="hidden w-24">
+        <img src={Logo} alt="logo" />
+      </div>
       <Nav />
     </header>
   );
