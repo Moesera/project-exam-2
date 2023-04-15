9ae5bdea2f5b01141f76933175b8574a70a0fcc0
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-15 02:32:34 +0200

    font fam, color, spacing rem

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      inder: ["Inder", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
     color: {
-   "pale-white": "#FDFDFD",
-   "gray": "#CBCBCB",
-   "stone-gray": "#D8D8D8",
-   "green": "#94EF69",
-   "red": "#CD3C3C",
-   "blue": "#1C6ECD"
-
+      "pale-white": "#FDFDFD",
+      gray: "#CBCBCB",
+      "stone-gray": "#D8D8D8",
+      green: "#94EF69",
+      red: "#CD3C3C",
+      blue: "#1C6ECD",
     },
     extend: {
       spacing: {
-        "16.5": "4.5rem",
+        16.5: "4.5rem",
         "3/7": "75%",
         "4/7": "85%",
         "5/7": "95%",
-        "11/12": "90%"
+        "11/12": "90%",
       },
     },
   },
