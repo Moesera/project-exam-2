f4c0e9bf040d5edd4df5906a6a3e1c6af3918a39
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-15 02:31:52 +0200

    images footer

--- src/layout/Nav/index.jsx
+++ src/layout/Nav/index.jsx
@@ -2,13 +2,16 @@ import { Link } from "react-router-dom";
 
 import SearchIcon from "../../assets/interface/icons8-search-30.png";
 import FilterIcon from "../../assets/interface/icons8-tune-50.png";
+import BookingIcon from "../../assets/interface/icons8-booking-80.png";
+import ExploreIcon from "../../assets/interface/icons8-compass-64.png";
+import ProfileIcon from "../../assets/interface/icons8-male-user-64.png";
 
 function Nav() {
   return (
     <>
-      <container className="flex items-center justify-center w-5/7">
+      <section className="flex items-center justify-center w-5/7">
         <input className="h-16.5 px-16 border rounded-full border-stone-grey w-full" type="text" placeholder="Search venues" />
-        <label className="absolute flex items-center justify-between pl-1 pr-1 w-5/7">
+        <label className="absolute flex items-center justify-between pl-3.5 pr-3.5 w-5/7">
           <div>
             <img className="w-10" src={SearchIcon} alt="search" />
           </div>
@@ -16,15 +19,19 @@ function Nav() {
             <img className="w-9" src={FilterIcon} alt="filter" />
           </div>
         </label>
-      </container>
+      </section>
       <nav className="flex self-center justify-center">
-        <ul className="fixed inset-x-0 bottom-0 bg-slate-400">
-          <li>
-            <img src="#" alt="bookings img" />
+        <ul className="fixed inset-x-0 bottom-0 flex justify-between m-auto w-4/7 bg-slate-400">
+          <li className="flex flex-col items-center">
+            <img className="w-16" src={ExploreIcon} alt="explore img" />
+            Explore
+          </li>
+          <li className="flex flex-col items-center">
+            <img className="w-16" src={BookingIcon} alt="bookings img" />
             bookings
           </li>
-          <li>
-            <img src="#" alt="profile img" />
+          <li className="flex flex-col items-center">
+            <img className="w-16" src={ProfileIcon} alt="profile img" />
             <Link>profile</Link>
           </li>
         </ul>
