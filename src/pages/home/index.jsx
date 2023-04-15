56e36d7f645b6e9f23c2eafc5ada7373259ef3e5
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-15 02:32:03 +0200

    homepage categories

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,14 @@
 
+import { categoryList } from "../../js/components/Categories/list";
+import Categories from "../../js/components/Categories";
 
 function Home() {
   return (
-    <h1 className="text-2xl font-bold underline">
-      Home
-    </h1>
-  )
+    <main>
+      <h1 className="text-2xl font-bold underline">Home</h1>
+      <Categories categories={categoryList} />
+    </main>
+  );
 }
 
 export default Home;
