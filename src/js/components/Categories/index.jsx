18a0f2e82d48f7f7cd70de32d89ff74f15761046
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-16 15:14:06 +0200

    Revert "overflow auto for categories"
    This reverts commit 65f881410c1e1c5676a244f30bd947e2d54718af.

--- src/js/components/Categories/index.jsx
+++ src/js/components/Categories/index.jsx
@@ -3,18 +3,16 @@
 
 function Categories({ categories }) {
   return (
-    <div className="overflow-auto w-3.5/7">
-    <ul className="flex gap-2 overflow-auto font-inder">
+    <ul className="flex gap-6 font-inder">
       {categories.map((category) => {
         return (
-          <li key={category.id} className="flex flex-col items-center grow min-w-[6rem] max-w-[6rem] p-1 rounded-lg hover:cursor-pointer hover:shadow-inner">
+          <li key={category.id} className="flex flex-col items-center">
             <img className="w-14" src={category.image} alt={category.title}/>
-            <p className="text-base">{category.title}</p>
+            <p>{category.title}</p>
           </li>
         )
       })}
     </ul>
-    </div>
   )
 }
 
