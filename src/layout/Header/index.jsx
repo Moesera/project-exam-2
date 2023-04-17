45b1d9f4b96df088ea9734b413a8a4f98206acd9
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-17 12:07:07 +0200

    scaling to desktop

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -4,7 +4,7 @@ import Logo from "../../assets/images/logo/icons8-floating-island-beach-100.png"
 
 function Header() {
   return (
-    <header className="fixed inset-x-0 flex flex-col items-center justify-center gap-2 py-6 border shadow-md bg-stone-50">
+    <header className="fixed inset-x-0 flex flex-col items-center justify-center gap-2 md:gap-4 py-6 bg-[#FDFDFD] border shadow-md">
       <div className="hidden w-24">
         <img src={Logo} alt="logo" />
       </div>
--- src/layout/Nav/index.jsx
+++ src/layout/Nav/index.jsx
@@ -5,6 +5,7 @@ import FilterIcon from "../../assets/interface/icons8-tune-50.png";
 import BookingIcon from "../../assets/interface/icons8-booking-80.png";
 import ExploreIcon from "../../assets/interface/icons8-compass-64.png";
 import ProfileIcon from "../../assets/interface/icons8-male-user-64.png";
+import ArrowIcon from "../../assets/interface/icons8-chevron-right.png";
 
 import { categoryList } from "../../js/components/Categories/list";
 import Categories from "../../js/components/Categories";
@@ -12,37 +13,49 @@ import Categories from "../../js/components/Categories";
 function Nav() {
   return (
     <>
-    
-    <section className="flex w-3.5/7 items-center">
+      {/* Search */}
+      <section className="flex w-3.5/7 items-center">
         <input className="pl-20 h-16.5 border rounded-full border-stone-grey w-full shadow-md" type="text" placeholder="Search venues" />
-        <label className="absolute flex justify-between w-3.5/7 pl-6 pr-6 items-center">
+        <label className="absolute flex items-center justify-between pl-6 pr-6">
           <div>
             <img className="w-10" src={SearchIcon} alt="search" />
           </div>
-          <div className="p-2 border rounded-full hover:shadow-inner hover:cursor-pointer">
-            <img className="w-9" src={FilterIcon} alt="filter" />
-          </div>
         </label>
+        <div className="absolute p-2 border rounded-full right-[12%] hover:shadow-inner hover:cursor-pointer md:hidden">
+          <img className="w-9" src={FilterIcon} alt="filter" />
+        </div>
+        {/* Navbar */}
+        <nav className="fixed md:static md:w-2/4 inset-x-0 bottom-0 flex self-center justify-center p-4 md:p-0 border md:border-none border-stone-300 font-inder bg-[#FDFDFD]">
+          <ul className="flex justify-between w-11/12 m-auto md:gap-4 md:justify-end x-sm:w-3/7 sm:w-3/5 md:w-full lg:gap-8">
+            <li className="flex flex-col items-center min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
+              <img className="w-12" src={ExploreIcon} alt="explore img" />
+              <Link to="home" className="text-base">
+                Explore
+              </Link>
+            </li>
+            <li className="flex flex-col items-center min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
+              <img className="w-12" src={BookingIcon} alt="bookings img" />
+              <Link className="text-base">Bookings</Link>
+            </li>
+            <li className="flex flex-col items-center min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
+              <img className="w-12" src={ProfileIcon} alt="profile img" />
+              <Link to="profile" className="text-base">
+                Profile
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </section>
-
-      <Categories categories={categoryList} />
-
-      <nav className="fixed inset-x-0 bottom-0 flex self-center justify-center p-4 border border-stone-300 font-inder bg-stone-50">
-        <ul className="flex justify-between w-11/12 m-auto">
-          <li className="flex flex-col items-center min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
-            <img className="w-12" src={ExploreIcon} alt="explore img" />
-            <Link to="home" className="text-base">Explore</Link>
-          </li>
-          <li className="flex flex-col items-center min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
-            <img className="w-12" src={BookingIcon} alt="bookings img" />
-            <Link className="text-base">Bookings</Link>
-          </li>
-          <li className="flex flex-col items-center min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
-            <img className="w-12" src={ProfileIcon} alt="profile img" />
-            <Link to="profile" className="text-base">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+      {/* Categories */}
+      <section className="w-3.5/7 flex items-center gap-4">
+        <Categories categories={categoryList} />
+        <div className="hidden border rounded-full hover:shadow-inner hover:cursor-pointer md:block">
+          <img className="w-12" src={ArrowIcon} alt="arrow right" />
+        </div>
+        <div className="hidden px-4 py-1 border rounded-full md:block hover:shadow-inner hover:cursor-pointer">
+          <p>filter</p>
+        </div>
+      </section>
     </>
   );
 }
