96eba7414dd375adee61c9a7315ade736b80d657
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-17 12:07:28 +0200

    distance from header

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 
 function Home() {
   return (
-    <main className="pt-64 bg-stone-50">
+    <main className="pt-72 bg-[#FDFDFD] w-3.5/7 mx-auto">
       <h1 className="text-2xl font-bold underline">Home</h1>
     </main>
   );
