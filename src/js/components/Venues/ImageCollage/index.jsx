20e2688f76af94dd8ef957d763325f73dbc5fd50
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-20 17:04:11 +0200

    images and uuid for images

--- src/js/components/Venues/ImageCollage/index.jsx
+++ src/js/components/Venues/ImageCollage/index.jsx
@@ -1,16 +1,16 @@
-function ImageCollage({images, name}) {
-  
-  return (
-    <div>
-      {images.map((img) => {
-        return (
-          <figure key={img}>
-            <img src={img} alt={name} />
-          </figure>
-        );
-      })}
-    </div>
-  );
+import PlaceholderImg from "../../../../assets/images/placeholder/item-placeholder.png";
+import { v4 as uuidv4 } from "uuid";
+
+const handleImgError = (e) => {
+  e.target.src = PlaceholderImg;
+};
+
+function ImageCollage({ images, name }) {
+    return (
+      <div className="">
+        <img className="object-cover shadow-lg aspect-3/2 rounded-xl" key={uuidv4()} src={images} onError={handleImgError} alt={name} />
+      </div>
+    );
 }
 
 export default ImageCollage;
