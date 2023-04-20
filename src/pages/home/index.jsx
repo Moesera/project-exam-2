e114e6ca04a58243b79a03f3195fe6c17ea20606
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-20 17:05:07 +0200

    h1 for seo hidden

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,7 +3,7 @@ import Venues from "../../js/components/Venues";
 function Home() {
   return (
     <main className="pt-72 bg-[#FDFDFD] w-3.5/7 mx-auto">
-      <h1 className="text-2xl font-bold underline">Home</h1>
+      <h1 className="sr-only">Homepage</h1>
 
       <Venues />
     </main>
