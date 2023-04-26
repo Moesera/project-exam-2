64f5f9b0987464ba658aec686ee4412c9f24a6e0
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-04-26 14:36:13 +0200

    placeholder function on error

--- src/js/components/Venues/ShowcaseImage/index.jsx
+++ src/js/components/Venues/ShowcaseImage/index.jsx
@@ -1,14 +1,10 @@
-import PlaceholderImg from "../../../../assets/images/placeholder/item-placeholder.png";
 import { v4 as uuidv4 } from "uuid";
-
-const handleImgError = (e) => {
-  e.target.src = PlaceholderImg;
-};
+import { handleItemImgError } from "../../../helpers/placeholder.js"
 
 function ShowcaseImage({ images, name }) {
     return (
       <div className="">
-        <img className="object-cover shadow-lg aspect-3/2 rounded-xl" key={uuidv4()} src={images} onError={handleImgError} alt={name} />
+        <img className="object-cover shadow-lg aspect-3/2 rounded-xl" key={uuidv4()} src={images} onError={handleItemImgError} alt={name} />
       </div>
     );
 }
