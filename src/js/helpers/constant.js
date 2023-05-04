1a11187c1cbe813de65178acc6b6035f03c81636
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-04 12:49:52 +0200

    constants for bookings
    also updated URLS_REQ_AUTH

--- src/js/helpers/constant.js
+++ src/js/helpers/constant.js
@@ -4,5 +4,6 @@ export const venues = `${api}venues/`;
 export const profile = `${api}profiles/`;
 export const register = `${api}auth/register`;
 export const login = `${api}auth/login`;
+export const bookings = `${api}bookings?_customer=true?_venue=true/`;
 
-export const URLS_REQ_AUTH = [register, login];
+export const URLS_REQ_AUTH = [profile, bookings];
