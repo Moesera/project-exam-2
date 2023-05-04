72ce9c1e38fea503eca2a038946f1154a6f6fdff
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-04 12:53:24 +0200

    profile button update
    added the new profile button

--- src/layout/Nav/index.jsx
+++ src/layout/Nav/index.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { closeModal, openModal } from "../../js/helpers/modal";
+import { closeModal } from "../../js/helpers/modal";
 
 // images
 import SearchIcon from "../../assets/interface/icons8-search-30.png";
 import FilterIcon from "../../assets/interface/icons8-tune-50.png";
 import BookingIcon from "../../assets/interface/icons8-booking-80.png";
 import ExploreIcon from "../../assets/interface/icons8-compass-64.png";
-import ProfileIcon from "../../assets/interface/icons8-male-user-64.png";
 import ArrowIcon from "../../assets/interface/icons8-chevron-right.png";
 
 // components
@@ -16,26 +15,31 @@ import Categories from "../../js/components/Categories";
 import PopupModal from "../../js/components/Modal/index";
 import RegisterForm from "../../js/components/Register";
 import LoginForm from "../../js/components/Login";
+import ProfileButton from "../../js/components/ProfileButton";
 
 function Nav() {
   const [components, setComponents] = useState([LoginForm, RegisterForm]);
   const [open, setOpen] = useState(false);
   const [showLogin, setShowLogin] = useState(true);
+  // export this or set / get this in Nav to pass it down to the profile component
+  
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
+  console.log(isLoggedIn);
 
   return (
     <>
       <PopupModal show={showLogin} setShow={setShowLogin} components={components} setComponents={setComponents} setOpen={setOpen} open={open} />
       {/* Search */}
       <section className="flex w-3.5/7 items-center xl:w-desktop">
-          <input className="pl-20 h-16.5 border rounded-full border-stone-grey w-full shadow-md" type="text" placeholder="Search venues" />
-          <label className="absolute flex items-center justify-between pl-6 pr-6">
-            <div>
-              <img className="w-10" src={SearchIcon} alt="search" />
-            </div>
-          </label>
-          <div className="absolute p-2 border rounded-full right-[12%] hover:shadow-inner hover:cursor-pointer md:hidden">
-            <img className="w-9" src={FilterIcon} alt="filter" />
+        <input className="pl-20 h-16.5 border rounded-full border-stone-grey w-full shadow-md" type="text" placeholder="Search venues" />
+        <label className="absolute flex items-center justify-between pl-6 pr-6">
+          <div>
+            <img className="w-10" src={SearchIcon} alt="search" />
           </div>
+        </label>
+        <div className="absolute p-2 border rounded-full right-[12%] hover:shadow-inner hover:cursor-pointer md:hidden">
+          <img className="w-9" src={FilterIcon} alt="filter" />
+        </div>
         {/* Navbar */}
         <nav className="fixed inset-x-0 bottom-0 flex self-center justify-center p-4 bg-white border md:static md:w-2/4 md:p-0 md:border-none border-light-gray font-inder">
           <ul className="flex justify-between w-11/12 m-auto md:gap-4 md:justify-end x-sm:w-3/7 sm:w-3/5 md:w-full lg:gap-8">
@@ -51,12 +55,7 @@ function Nav() {
                 Bookings
               </Link>
             </li>
-            <li className="min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
-              <Link onClick={() => openModal({ setOpen, setShowLogin })} className="flex flex-col items-center text-base ">
-                <img className="w-12" src={ProfileIcon} alt="profile img" />
-                Profile
-              </Link>
-            </li>
+            <ProfileButton setOpen={setOpen} setShow={setShowLogin} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
           </ul>
         </nav>
       </section>
