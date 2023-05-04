03d780e085abd95f84a44221284d722649b474cf
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-04 12:42:17 +0200

    api authentication

--- src/js/authentication/apiAuth.js
+++ src/js/authentication/apiAuth.js
@@ -1,44 +1,15 @@
-import React, { useState } from 'react';
-
-export function Auth() {
-  // export this or set / get this in Nav to pass it down to the profile component
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
-
-  // maybe export a function like this for login and logout to set 
-  // login false and delete elements in local storage
-  function handleLogin() {
-    // Handle login
-    setIsLoggedIn(true);
-  }
-
-  // Use is logged in to render the this to the profile text.
-  return (
-    <div>
-      {isLoggedIn ? (
-        <>
-          {/* Show content for logged-in users */}
-          <a href="/profile">Profile</a>
-        </>
-      ) : (
-        <>
-          {/* Show content for non-logged-in users */}
-          <button onClick={handleLogin}>Login</button>
-        </>
-      )}
-    </div>
-  );
-}
+import { URLS_REQ_AUTH } from '../helpers/constant';
 
 // use this to check for the urls that needs auth check, check the token
 // return the login modal or a toaster to notify the user that they need to login if not valid.
 export function checkAuthAndFetch(url) {
-  const urlsThatRequireAuth = ['/data1', '/data2'];
-  if (urlsThatRequireAuth.includes(url) && !localStorage.getItem('apiAuth')) {
+
+  if (URLS_REQ_AUTH.includes(url) && !localStorage.getItem('token')) {
     // Show login popup or error message
-    // Change profile link to login link
-    document.getElementById('profile-link').textContent = 'Login';
-    document.getElementById('profile-link').href = '/login';
+    // relocate the user to the homepage and then open the login modal
+    return false;
   } else {
     // Proceed to fetch data
+    return true;
   }
 }
