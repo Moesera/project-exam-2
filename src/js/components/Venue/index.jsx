9f911eef4c4d9cca4e77d6fb4da5fed03f92bf69
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-08 15:13:57 +0200

    venue styling and splitter for description

--- src/js/components/Venue/index.jsx
+++ src/js/components/Venue/index.jsx
@@ -12,7 +12,7 @@ function Venue({ venueData }) {
   return (
     <>
       <section className="my-60 w-3.5/7 mx-auto xl:w-desktop text-xl md-sm:text-2xl">
-      <div className="hover:underline hover:cursor-pointer" onClick={goBack}>Back</div>
+      <div className="my-4 hover:underline hover:cursor-pointer" onClick={goBack}>Back</div>
         <SliderComponent images={venueData.media} name={venueData.name} />
         <div className="flex items-center justify-between mt-2">
           <h2 className="mb-2 font-semibold sm:truncate">{venueData.name}</h2>
@@ -27,7 +27,7 @@ function Venue({ venueData }) {
         <div className="flex flex-col gap-2">
         <LocationComponent location={venueData.location} id={venueData.id}/>
           <p className="mb-2">Date: {venueData.updated}</p>
-          <p>{venueData.description}</p>
+          <p className="pt-2 border-t border-gray">{venueData.description}</p>
 
           <section className="fixed inset-x-0 p-6 border bottom-[7.5rem] border-light-gray bg-white z-50 md:bottom-0">
             <div className="flex items-center justify-between mx-auto w-4/7 xl:w-desktop">
