d5839b6b6dd0cfc608e70cd9d448c501e0eb22c2
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-10 21:59:51 +0200

    edit venues and form

---
+++ src/js/components/EditVenue/index.jsx
@@ -0,0 +1,22 @@
+import Venues from "../Venues";
+
+function EditVenue({ setActiveComponent, data }) {
+
+  function goBack() {
+    setActiveComponent("default");
+  }
+
+  console.log(data);
+
+  return (
+    <main className="pt-72 bg-[#FDFDFD] w-3.5/7 mx-auto xl:w-desktop mb-14">
+      <div className="hover:underline hover:cursor-pointer" onClick={goBack}>
+        Back
+      </div>
+      <h2 className="w-11/12 mt-10 mb-12 text-4xl font-medium">Your venues</h2>
+      <Venues data={data} />
+    </main>
+  );
+}
+
+export default EditVenue;
