dfd53ff27ffe40a7322aa31c355d8f528152d270
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-12 00:18:16 +0200

    Added booking modal to store

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modalSlice from "./js/hooks/modal";
 import userAuthSlice from "./js/hooks/userAuth";
+import bookingSlice from "./js/hooks/bookingModal";
 
 export const store = configureStore({
   reducer: {
     // Handles the open and closing of the modal
     modal: modalSlice,
+
+    booking: bookingSlice,
     // checks if the user is logged inn or not.
     userAuth: userAuthSlice,
   },
