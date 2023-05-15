8e662d40e567314c6bb9f7e8b921552056a8cf16
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-15 15:41:20 +0200

    booking component and disableDates

--- src/js/components/Booking/index.jsx
+++ src/js/components/Booking/index.jsx
@@ -1,21 +1,80 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import DatePicker, { CalendarContainer } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useDispatch } from "react-redux";
-import { closeBooking } from "../../hooks/bookingModal";
 
-function Booking() {
+import { useApi } from "../../hooks/service/api";
+import { closeBooking } from "../../hooks/bookingModal";
+import { bookings } from "../../helpers/constant";
+import { useParams } from "react-router-dom";
+
+const schema = yup
+  .object({
+    guests: yup.number().positive().integer().typeError("please enter a number").required(),
+  })
+  .required();
+
+function Booking({ venueId, venueGuests, bookingsArray }) {
   const dispatch = useDispatch();
-  // const [startDate, setStartDate] = useState(null);
+  const { id } = useParams();
+  const { apiData, isSuccess, isError } = useApi(bookings + id);
   const [dateRange, setDateRange] = useState([null, null]);
   const [startDate, endDate] = dateRange;
-  // const [endDate, setEndDate] = useState(null);
+  const [disableDates, setDisableDates] = useState([]);
+
+  const handleBookingArray = useCallback((bookingsArray) => {
+    const dates =  bookingsArray.map((booking) => {
+      const dateFrom = new Date(booking.dateFrom);
+      const dateTo = new Date(booking.dateTo);
+      return getDatesBetween(dateFrom, dateTo);
+      // disableDates = [...disableDates, ...dates];
+      // console 39 disable dates
+    });
+    setDisableDates(dates.flat());
+  }, [])
+
+  useEffect(() => {
+    if (bookingsArray) {
+      handleBookingArray(bookingsArray);
+    }
+  }, [bookingsArray, handleBookingArray]);
+
+  function getDatesBetween(startDate, endDate) {
+    const dates = [];
+    let currentDate = startDate;
+    while (currentDate <= endDate) {
+      dates.push(new Date(currentDate));
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    return dates;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit(data) {
+    const method = "POST";
+    const updatedData = { ...data, dateFrom: startDate, dateTo: endDate, venueId: venueId };
+    console.log(updatedData, method);
+    apiData(updatedData, method);
+    setTimeout(() => {
+      dispatch(closeBooking())
+    }, 2500)
+  }
 
   function FromDate({ className, children }) {
     return (
       <div style={{ color: "#fff" }}>
         <CalendarContainer className={className}>
-          <div style={{ position: "relative" }}>{children}</div>
+          <div style={{ position: "relative", width: "20rem", fontSize: "1.2rem" }}>{children}</div>
         </CalendarContainer>
       </div>
     );
@@ -29,7 +88,7 @@ function Booking() {
         </button>
       </div>
       <h2>Booking</h2>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
         <DatePicker
           selectsRange={true}
           onChange={(update) => {
@@ -38,13 +97,14 @@ function Booking() {
           selectsEnd
           startDate={startDate}
           endDate={endDate}
-          minDate={startDate}
+          minDate={new Date()}
           disablePast="true"
           isClearable
           placeholderText="select a date"
           closeOnScroll={true}
           calendarContainer={FromDate}
           // withPortal
+          excludeDates={disableDates}
           showWeekNumbers
           fixedHeight
           showMonthDropdown
@@ -52,11 +112,13 @@ function Booking() {
         />
         <label className="flex flex-col">
           guests
-          <input className="p-2 border rounded-lg" type="number"/>
+          <input {...register("guests")} className="p-2 border rounded-lg" type="number" defaultValue={1} min={1} max={venueGuests} />
         </label>
-        <div>
+        <div className="">
           <button className="flex-1 px-4 py-2 border rounded-lg bg-ocean hover:cursor-pointer">Book</button>
         </div>
+        {isError && <div className="error">{isError}</div>}
+        {isSuccess && <div className="success">Venue was successfully booked</div>}
       </form>
     </section>
   );
