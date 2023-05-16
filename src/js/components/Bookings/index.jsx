afe817f1b1c3731a6a988f92dc6de03f8a52ee3a
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-16 18:23:54 +0200

    bookings delete and check venue

--- src/js/components/Bookings/index.jsx
+++ src/js/components/Bookings/index.jsx
@@ -1,17 +1,41 @@
 import { Link, useLocation } from "react-router-dom";
-import chevronRight from "../../../assets/interface/icons8-chevron-right.png";
+// import { useEffect } from "react";
 
 import BookingDate from "../BookingDate";
+import { useApi } from "../../hooks/service/api";
+import { bookings } from "../../helpers/constant";
+import { useState } from "react";
 
 function UserBookings({ setActiveComponent, data }) {
-let location = useLocation()
+  const location = useLocation();
+  const [bookingsData, setBookingsData] = useState(data)
 
-console.log(data)
+
+  function DeleteButton({id, setBookingsData}) {
+    const { apiData } = useApi(bookings + id);
+
+    function handleDeleteBooking() {
+      const method = "DELETE";
+      apiData(null, method);
+
+      const updatedBookingsData = bookingsData.filter((bookings) => bookings.id !== id);
+      setBookingsData(updatedBookingsData);
+    }
+
+    return (
+      <button
+        type="button"
+        onClick={() => handleDeleteBooking(id)}
+        className="invisible p-2 font-medium border rounded-lg hover:bg-opacity-80 bg-error hover:cursor-pointer group-hover/item:visible"
+      >
+        Delete
+      </button>
+    );
+  }
+
   function goBack() {
     setActiveComponent("default");
   }
-
-
 
   return (
     <main className="pt-72 bg-[#FDFDFD] w-3.5/7 mx-auto xl:w-desktop mb-14">
@@ -19,19 +43,27 @@ console.log(data)
         Back
       </div>
       <h1 className="py-10 mt-12 text-4xl font-medium">Bookings</h1>
-      {data ? (
+      {bookingsData ? (
         <section className="flex flex-col gap-4">
-          {data.map((bookings) => (
-            <Link to={{ pathname: `/venue/${bookings.venue.id}`, state: {from: location} }} key={bookings.id} className="flex items-center justify-between min-h-[10rem] gap-2 px-4 py-2 rounded-xl shadow-3xl group/item hover:cursor-pointer">
+          {bookingsData.map((bookings) => (
+            <div key={bookings.id} className="flex items-center justify-between min-h-[10rem] gap-2 px-4 py-2 rounded-xl shadow-3xl group/item hover:cursor-pointer">
               <div>
-                <h2 className="mb-2 font-semibold sm:truncate">{bookings.venue.name}</h2>
+                <div className="flex justify-between">
+                  <h2 className="mb-2 font-semibold sm:truncate">{bookings.venue.name}</h2>
+                </div>
                 <p>{bookings.guests} Guests</p>
-              <BookingDate dateFrom={bookings.dateFrom} dateTo={bookings.dateTo}/>
+                <BookingDate dateFrom={bookings.dateFrom} dateTo={bookings.dateTo} />
               </div>
-              <figure className="ml-auto w-[3rem] md-sm:w-14">
-                <img className="invisible object-cover w-full h-full group-hover/item:visible" src={chevronRight} alt={"chevron right"} />
-              </figure>
-            </Link>
+              <div className="flex flex-col gap-4">
+                <DeleteButton id={bookings.id} setBookingsData={setBookingsData} />
+                <Link
+                  to={{ pathname: `/venue/${bookings.venue.id}`, state: { from: location } }}
+                  className="invisible p-2 font-medium border rounded-lg hover:bg-opacity-80 bg-success hover:cursor-pointer group-hover/item:visible"
+                >
+                  Venue
+                </Link>
+              </div>
+            </div>
           ))}
         </section>
       ) : (
