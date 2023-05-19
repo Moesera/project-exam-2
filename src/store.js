2d0dae25d9f40d75bfcdfe240a4c07eef9b143b7
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-19 14:05:33 +0200

    added search to configure store reducers

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import modalSlice from "./js/hooks/modal";
 import userAuthSlice from "./js/hooks/userAuth";
 import bookingSlice from "./js/hooks/bookingModal";
+import searchSlice from "./js/hooks/search/search";
 
 export const store = configureStore({
   reducer: {
@@ -9,7 +10,11 @@ export const store = configureStore({
     modal: modalSlice,
 
     booking: bookingSlice,
+    
     // checks if the user is logged inn or not.
     userAuth: userAuthSlice,
+
+    //  search functionality
+    searchItem: searchSlice,
   },
 });
