20299b9f66e85a52f3d38dec9a8952594084fa49
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-19 16:02:18 +0200

    handle search function

--- src/layout/Nav/index.jsx
+++ src/layout/Nav/index.jsx
@@ -1,7 +1,8 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { closeModal } from '../../js/hooks/modal';
+import { useSelector, useDispatch } from "react-redux";
+import { closeModal } from "../../js/hooks/modal";
+import { setSearchInput } from "../../js/hooks/search/search";
 
 // images
 import SearchIcon from "../../assets/interface/icons8-search-30.png";
@@ -19,23 +20,29 @@ import LoginForm from "../../js/components/Login";
 import ProfileButton from "../../js/components/ProfileButton";
 
 function Nav() {
-  const isModalOpen = useSelector(state => state.modal?.isOpen);
+  const isModalOpen = useSelector((state) => state.modal?.isOpen);
   const isLoggedIn = useSelector((state) => state.userAuth?.isLoggedIn);
+  const searchInput = useSelector((state) => state.search.searchInput);
   const dispatch = useDispatch();
 
   const components = [LoginForm, RegisterForm];
   const [showLogin, setShowLogin] = useState(true);
 
+  function handleSearch(e) {
+      const input = e.target.value;
+      dispatch(setSearchInput(input));
+  }
+
   return (
     <>
-    {isModalOpen && <PopupModal show={showLogin} setShow={setShowLogin} components={components} open={isModalOpen} />}
+      {isModalOpen && <PopupModal show={showLogin} setShow={setShowLogin} components={components} open={isModalOpen} />}
       {/* Search */}
       <section className="flex w-3.5/7 items-center xl:w-desktop">
-        <input className="pl-20 h-16.5 rounded-full bg-white w-full shadow-3xl" type="text" placeholder="Search venues" />
+        <input defaultValue={searchInput} onKeyDown={handleSearch} className="pl-20 h-16.5 rounded-full bg-white w-full shadow-3xl" type="text" placeholder="Search venues" />
         <label className="absolute flex items-center justify-between pl-6 pr-6">
-          <div>
+          <Link to="/">
             <img className="w-10" src={SearchIcon} alt="search" />
-          </div>
+          </Link>
         </label>
         <div className="absolute p-2 border rounded-full right-[12%] hover:shadow-inner hover:cursor-pointer md:hidden">
           <img className="w-9" src={FilterIcon} alt="filter" />
@@ -43,13 +50,13 @@ function Nav() {
         {/* Navbar */}
         <nav className="fixed inset-x-0 bottom-0 flex self-center justify-center p-4 bg-white border md:static md:w-2/4 md:p-0 md:border-none border-light-gray font-inder">
           <ul className="flex justify-between w-11/12 m-auto md:gap-4 md:justify-end x-sm:w-3/7 sm:w-3/5 md:w-full lg:gap-8">
-            <li className="min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
+            <li className="min-w-[5rem] p-2 hover:cursor-pointer border-b-4 hover:border-gray border-white">
               <Link onClick={() => dispatch(closeModal())} to="/" className="flex flex-col items-center text-base">
                 <img className="w-12" src={ExploreIcon} alt="explore img" />
                 Explore
               </Link>
             </li>
-            <li className="min-w-[5rem] p-2 rounded-lg hover:cursor-pointer hover:shadow-inner">
+            <li className="min-w-[5rem] p-2 hover:cursor-pointer border-b-4 hover:border-gray border-white">
               <Link onClick={() => dispatch(closeModal())} className="flex flex-col items-center text-base">
                 <img className="w-12" src={BookingIcon} alt="bookings img" />
                 Bookings
