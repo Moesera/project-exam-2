66ccb3dbf6c1e5953fc969c65d50d718cf09e31a
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-19 16:02:35 +0200

    search function for homepage.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setFilteredData } from "../../js/hooks/search/search";
+
 import { useGet } from "../../js/hooks/service/get";
 import { venues } from "../../js/helpers/constant";
 
@@ -5,10 +9,19 @@ import Venues from "../../js/components/Venues";
 
 function Home() {
   const { data, isLoading, isError } = useGet(venues);
+  const dispatch = useDispatch();
+  const searchInput = useSelector((state) => state.search?.searchInput);
+  const filteredData = useSelector((state) => state.search.filteredData);
+
+  useEffect(() => {
+    const filteredData = data.filter((item) => item.name.toLowerCase().includes(searchInput.toLowerCase()));
+    dispatch(setFilteredData(filteredData));
+  }, [data, searchInput, dispatch]);
+
   return (
     <main className="pt-72 bg-[#FDFDFD] w-3.5/7 mx-auto xl:w-desktop mb-14">
       <h1 className="sr-only">Homepage</h1>
-      <Venues data={data} isLoading={isLoading} isError={isError}/>
+      <Venues data={filteredData} isLoading={isLoading} isError={isError} />
     </main>
   );
 }
