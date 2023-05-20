5638c2de8ee09147d721c2dd576a5d6e7613426b
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-20 11:08:37 +0200

    date converter function

---
+++ src/js/helpers/convertDate.js
@@ -0,0 +1,7 @@
+export function convertDate(dates) {
+  const date = new Date(dates);
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  return formattedDate;
+}
