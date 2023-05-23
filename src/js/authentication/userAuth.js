f21dc60c082f01fd3603b510ab8d89cea0586598
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-23 22:13:53 +0200

    format

--- src/js/authentication/userAuth.js
+++ src/js/authentication/userAuth.js
@@ -1,11 +1,10 @@
 import { getItem } from "../localStorage/getItem";
-import { setIsLoggedIn } from "../hooks/userAuth"
+import { setIsLoggedIn } from "../hooks/userAuth";
 
 export function handleUserAuth(dispatch) {
-if(getItem("token")) {
-dispatch(setIsLoggedIn(true));
-} else {
-  dispatch(setIsLoggedIn(false));
+  if(getItem("token")) {
+    dispatch(setIsLoggedIn(true));
+  } else {
+    dispatch(setIsLoggedIn(false));
+  }
 }
-}
-
