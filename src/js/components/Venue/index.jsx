fb2601743d7c6f6980b0785dc1f49c74ca36a04f
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-24 17:15:25 +0200

    margin-y

--- src/js/components/Venue/index.jsx
+++ src/js/components/Venue/index.jsx
@@ -52,7 +52,7 @@ function Venue({ venueData }) {
     <>
       {isModalOpen && <BookingModal open={isModalOpen} venueId={venueData.id} venueGuests={venueData.maxGuests} bookingsArray={venueData.bookings} />}
       <section className="pt-40 pb-20 w-3.5/7 mx-auto xl:w-desktop text-xl md-sm:text-2xl">
-        <div className="my-4 hover:underline hover:cursor-pointer" onClick={goBack}>
+        <div className="mb-4 hover:underline hover:cursor-pointer" onClick={goBack}>
           Back
         </div>
         <SliderComponent images={venueData.media} name={venueData.name} />
