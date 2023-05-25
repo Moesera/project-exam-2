7fec536ff899149d582ca292e5677cbd683de4aa
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-25 12:58:04 +0200

    fixed block comment redundant

--- src/js/authentication/apiAuth.js
+++ src/js/authentication/apiAuth.js
@@ -4,7 +4,7 @@ import { URLS_REQ_AUTH } from "../helpers/constant";
  * TODO Check if there is an authorization in application / localStorage instead.
  * ! use the non authorized endpoints to pass over the check..
  */
-{/* <Navigate to="/dashboard" replace={true} /> */}
+// <Navigate to="/dashboard" replace={true} />
 // use this to check for the urls that needs auth check, check the token
 // return the login modal or a toaster to notify the user that they need to login if not valid.
 export function checkAuthAndFetch(url) {
