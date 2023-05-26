9482056102a1927073f8c064a297f8e481d13cbe
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-26 18:56:11 +0200

    helmet provider wrapper

--- src/js/App.js
+++ src/js/App.js
@@ -1,24 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 import Layout from '../layout/Layout';
 import Home from '../pages/home';
 import Details from "../pages/details";
 import Profile from "../pages/profile";
+import Contact from '../pages/contact';
 import RouteNotFound from "../pages/error";
 import '../styles/App.css';
 
 function App() {
   return (
+    <HelmetProvider>
     <div className="App">
       <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="venue/:id" element={<Details />} />
         <Route path="profile/:name" element={<Profile />} />
+        <Route path="contact/" element={<Contact />} />
         <Route path="*" element={<RouteNotFound />} />
         </Route>
       </Routes>
     </div>
+    </HelmetProvider>
   );
 }
 
