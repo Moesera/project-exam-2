328fb9ce753d000c49e4f1cf507ba9c2ec831074
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-26 19:01:12 +0200

    proper loader

--- src/js/components/Venues/index.jsx
+++ src/js/components/Venues/index.jsx
@@ -8,7 +8,15 @@ import LocationComponent from "../Location";
 
 function Venues({ data, isLoading, isError }) {
   if (isLoading) {
-    return <div>Loading</div>;
+    return (
+      <div class="flex items-center justify-center min-h-screen p-5 bg-gray-100 min-w-screen">
+      <div class="flex space-x-2 animate-pulse">
+        <div class="w-3 h-3 bg-ocean rounded-full"></div>
+        <div class="w-3 h-3 bg-ocean rounded-full"></div>
+        <div class="w-3 h-3 bg-ocean rounded-full"></div>
+      </div>
+    </div>
+    );
   }
 
   if (isError) {
