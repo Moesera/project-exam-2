46f390c460cea9c3c84a2004812d449f92a724a4
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-27 15:58:19 +0200

    Link to contact form in profile

--- src/js/components/Card/index.jsx
+++ src/js/components/Card/index.jsx
@@ -1,7 +1,21 @@
-function CardComponent({setActiveComponent, id, title, content, media, alt }) {
+import { Link } from "react-router-dom";
 
+function CardComponent({ setActiveComponent, id, title, content, media, alt }) {
   function handleClick() {
     setActiveComponent(id);
+  }
+  if (id === "support") {
+    return (
+      <Link to="/contact" className="flex items-center justify-between min-h-[10rem] gap-2 px-4 py-2 rounded-xl shadow-3xl group/item hover:cursor-pointer">
+        <div className="text-lg md-sm:text-2xl basis-[75%] py-4">
+          <h2 className="font-semibold">{title}</h2>
+          <p className="group-hover/item:underline">{content}</p>
+        </div>
+        <figure className="w-[6rem] h-[6rem] md-sm:w-[7rem] md-sm:h-[7rem]">
+          <img className="object-cover w-full h-full" src={media} alt={alt} />
+        </figure>
+      </Link>
+    );
   }
 
   return (
