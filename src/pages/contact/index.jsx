e319e40e85ca05721c6e0a87fe74831cb3bbb158
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-28 17:12:20 +0200

    contact form restrictions and error handling

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,7 +1,37 @@
 import { Helmet } from "react-helmet-async";
 import { useGoBack } from "../../js/hooks/tools/useGoBack";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useState } from "react";
+
+const schema = yup
+  .object({
+    name: yup.string().min(3).required(),
+    email: yup.string().email().matches(/^[a-zA-Z0-9._%+-]+@stud.noroff.no$/, "Email must be of type @stud.noroff.no"),
+    subject: yup.string().min(4),
+    message: yup.string().min(10, "message must be at least 10 characters")
+    })
+  .required();
 
 function Contact() {
+  const [ isSuccess, setIsSuccess] = useState(false)
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  function onSubmit() {
+    setIsSuccess(true);
+    setTimeout(() => {
+    reset()
+    }, 1500);
+  }
+
 const goBack = useGoBack();
 
   return (
@@ -15,40 +45,45 @@ const goBack = useGoBack();
         Back
       </div>
         <h1 className="text-5xl font-medium text-center mb-14">Contact</h1>
-        <form className="font-inder" method="POST">
+        <form className="font-inder" onSubmit={handleSubmit(onSubmit)}>
           <div class="mb-5">
             <label for="name" p-2 class="mb-3 block font-medium">
               Full Name
             </label>
-            <input type="text" name="name" id="name" placeholder="Full Name" class="w-full p-2 border rounded-lg outline-none focus:border-ocean focus:shadow-md" />
+            <input {...register("name")} type="text" name="name" id="name" placeholder="Full Name" class="w-full p-2 border rounded-lg outline-none focus:border-ocean focus:shadow-md" />
+            <p className="error-message">{errors.name?.message}</p>
           </div>
           <div class="mb-5">
             <label for="email" class="mb-3 block font-medium ">
               Email Address
             </label>
-            <input type="email" name="email" id="email" placeholder="example@domain.com" class="w-full p-2 border rounded-lg outline-none focus:border-ocean focus:shadow-md" />
+            <input {...register("email")} type="email" name="email" id="email" placeholder="example@domain.com" class="w-full p-2 border rounded-lg outline-none focus:border-ocean focus:shadow-md" />
+            <p className="error-message">{errors.email?.message}</p>
           </div>
           <div class="mb-5">
             <label for="subject" class="mb-3 block font-medium">
               Subject
             </label>
-            <input type="text" name="subject" id="subject" placeholder="Enter your subject" class="w-full p-2 border rounded-lg outline-none focus:border-ocean focus:shadow-md" />
+            <input {...register("subject")}  type="text" name="subject" id="subject" placeholder="Enter your subject" class="w-full p-2 border rounded-lg outline-none focus:border-ocean focus:shadow-md" />
+            <p className="error-message">{errors.subject?.message}</p>
           </div>
           <div class="mb-5">
             <label for="message" class="mb-3 block font-medium">
               Message
             </label>
-            <textarea
+            <textarea {...register("message")}
               rows="4"
               name="message"
               id="message"
               placeholder="Type your message"
               class="p-2 rounded-lg w-full resize-none border bg-white font-medium outline-none focus:border-ocean focus:shadow-md"
             ></textarea>
+            <p className="error-message">{errors.message?.message}</p>
           </div>
           <div>
             <button class="hover:shadow-form p-2 border rounded-lg bg-success disabled:bg-gray outline-none">Submit</button>
           </div>
+          {isSuccess && <div className="mt-2 success">Message was successfully sent</div>}
         </form>
       </div>
     </main>
