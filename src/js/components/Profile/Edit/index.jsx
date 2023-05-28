8199ce6f1e10e61d9d66d690cab9ce424ef37f89
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-28 12:54:33 +0200

    formatting

--- src/js/components/Profile/Edit/index.jsx
+++ src/js/components/Profile/Edit/index.jsx
@@ -1,7 +1,6 @@
 import { useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
-// import { ToastContainer, toast } from "react-toastify";
 import { useDispatch } from "react-redux";
 import { closeModal } from "../../../hooks/modal";
 import { useApi } from "../../../hooks/service/api";
@@ -16,8 +15,10 @@ import { profile } from "../../../helpers/constant";
 export default function EditProfile() {
   const dispatch = useDispatch();
   const { name } = useParams();
-  const { apiData, isSuccess, isLoading, isError } = useApi(profile + name + "/media")
-  const { handleSubmit} = useForm();
+  const { apiData, isSuccess, isLoading, isError } = useApi(
+    profile + name + "/media"
+  );
+  const { handleSubmit } = useForm();
 
   const [count, setCount] = useState(0);
   const [chosenAvatar, setChosenAvatar] = useState(false);
@@ -26,10 +27,8 @@ export default function EditProfile() {
   const [avatars, setAvatars] = useState([]);
   const avatarContainerRef = useRef(null);
 
-
-
   function onSubmit() {
-    const data = {avatar: chosenAvatar}
+    const data = { avatar: chosenAvatar };
     const method = "PUT";
 
     // // notify();
@@ -55,7 +54,9 @@ export default function EditProfile() {
   }
 
   function handleAvatarChange() {
-    const avatarRadioButtons = avatarContainerRef.current.querySelectorAll(`input[type="radio"][name="avatar"]`);
+    const avatarRadioButtons = avatarContainerRef.current.querySelectorAll(
+      `input[type="radio"][name="avatar"]`
+    );
     let chosenAvatarValue;
     avatarRadioButtons.forEach((radioButton) => {
       if (radioButton.checked) {
@@ -65,39 +66,76 @@ export default function EditProfile() {
     setChosenAvatar(chosenAvatarValue);
   }
 
-  if(isLoading) {
+  if (isLoading) {
     return (
       <div class="flex items-center justify-center p-5 bg-gray-100">
-      <div class="flex space-x-2 animate-pulse">
-        <div class="w-3 h-3 bg-ocean rounded-full"></div>
-        <div class="w-3 h-3 bg-ocean rounded-full"></div>
-        <div class="w-3 h-3 bg-ocean rounded-full"></div>
+        <div class="flex space-x-2 animate-pulse">
+          <div class="w-3 h-3 bg-ocean rounded-full"></div>
+          <div class="w-3 h-3 bg-ocean rounded-full"></div>
+          <div class="w-3 h-3 bg-ocean rounded-full"></div>
+        </div>
       </div>
-    </div>
-    )
+    );
   }
 
   return (
     <section className="w-[95%] mx-auto mt-4">
-      <button type="button" className="px-2 mt-2 mb-2 font-medium border rounded-lg hover:opacity-90 bg-error" onClick={() => dispatch(closeModal())}>
+      <button
+        type="button"
+        className="px-2 mt-2 mb-2 font-medium border rounded-lg hover:opacity-90 bg-error"
+        onClick={() => dispatch(closeModal())}
+      >
         X
       </button>
-      <h1 className="w-11/12 mt-10 mb-12 text-4xl font-medium">Edit your profile</h1>
+      <h1 className="w-11/12 mt-10 mb-12 text-4xl font-medium">
+        Edit your profile
+      </h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col font-inder">
+        <div className="flex flex-col font-inder">
           Avatar
-          <div id="avatarContainer" ref={avatarContainerRef} className="flex gap-4" onChange={handleAvatarChange}>
+          <div
+            id="avatarContainer"
+            ref={avatarContainerRef}
+            className="flex gap-4"
+            onChange={handleAvatarChange}
+          >
             <label>
-              <input className="hidden" type="radio" id="preset" name="avatar" value={AvatarPlaceholder} />
+              <input
+                className="hidden"
+                type="radio"
+                id="preset"
+                name="avatar"
+                value={AvatarPlaceholder}
+              />
               <figure className="w-20 h-20 p-2 rounded-lg shadow-3xl hover:cursor-pointer hover:border hover:border-gray">
-                <img className="object-cover w-full h-full" src={AvatarPlaceholder} alt="placeholder" pattern="^(https?|ftp):\/\/(-\.)?([^\s/?\.#-]+\.?)+(\/[^\s]*)?$" required/>
+                <img
+                  className="object-cover w-full h-full"
+                  src={AvatarPlaceholder}
+                  alt="placeholder"
+                  pattern="^(https?|ftp):\/\/(-\.)?([^\s/?\.#-]+\.?)+(\/[^\s]*)?$"
+                  required
+                />
               </figure>
             </label>
-            <NewAvatar avatars={avatars} setAvatars={setAvatars} count={count} setCount={setCount} />
+            <NewAvatar
+              avatars={avatars}
+              setAvatars={setAvatars}
+              count={count}
+              setCount={setCount}
+            />
             {/*  */}
             {/* Add image button */}
-            <button onClick={setHidden} id="addAvatar" type="button" className={`w-20 p-2 rounded-lg shadow-3xl hover:border hover:border-gray transition-opacity duration-300`}>
-              <img className="object-cover w-full h-full transition-opacity duration-300" src={isHidden ? AddIcon : CancelIcon} alt="add" />
+            <button
+              onClick={setHidden}
+              id="addAvatar"
+              type="button"
+              className={`w-20 p-2 rounded-lg shadow-3xl hover:border hover:border-gray transition-opacity duration-300`}
+            >
+              <img
+                className="object-cover w-full h-full transition-opacity duration-300"
+                src={isHidden ? AddIcon : CancelIcon}
+                alt="add"
+              />
             </button>
             {/* End of image button */}
           </div>
@@ -128,7 +166,9 @@ export default function EditProfile() {
           Submit
         </button>
         {isError && <p className="error">Error: {isError}</p>}
-        {isSuccess && <div className="success">Venue was successfully updated</div>}
+        {isSuccess && (
+          <div className="success">Venue was successfully updated</div>
+        )}
       </form>
     </section>
   );
