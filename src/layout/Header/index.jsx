7a94b8ae17805d7c272a8ff44f1dc1f4bac43aec
Sigvel <89355439+Sigvel@users.noreply.github.com>
2023-05-28 21:50:45 +0200

    from class to classname

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -4,9 +4,9 @@ import CompassLogo from "../../assets/interface/icons8-compass-64.png";
 function Header() {
   return (
     <header className="fixed inset-x-0 z-50 flex flex-col items-center justify-center gap-2 py-6 bg-white shadow-3xl md:gap-4">
-      <p class="flex items-center mb-4 sm:mb-0">
-          <img src={CompassLogo} class="mr-4 h-12 w-12" alt="Compasso Logo" />
-          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Compasso</span>
+      <p className="flex items-center mb-4 sm:mb-0">
+          <img src={CompassLogo} className="mr-4 h-12 w-12" alt="Compasso Logo" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Compasso</span>
         </p>
       <Nav />
     </header>
