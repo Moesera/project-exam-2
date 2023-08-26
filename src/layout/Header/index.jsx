c2768e8e3d466d5dd7fded69911ae352c424b1da
Moesera <89355439+Moesera@users.noreply.github.com>
2023-08-26 14:37:11 +0200

    formatting

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -2,6 +2,7 @@ import Nav from "../Nav";
 import CompassLogo from "../../assets/interface/icons8-compass-64.png";
 
 function Header() {
+  
   return (
     <header className="fixed inset-x-0 z-50 flex flex-col items-center justify-center gap-2 py-6 bg-white shadow-3xl md:gap-4">
       <p className="flex items-center mb-4 sm:mb-0">
