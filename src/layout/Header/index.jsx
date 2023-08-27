54301846ecb72c965df70adb5e992bd48420fe23
Moesera <89355439+Moesera@users.noreply.github.com>
2023-08-27 20:48:02 +0200

    logo text: -dark:text-white

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -7,7 +7,7 @@ function Header() {
     <header className="fixed inset-x-0 z-50 flex flex-col items-center justify-center gap-2 py-6 bg-white shadow-3xl md:gap-4">
       <p className="flex items-center mb-4 sm:mb-0">
           <img src={CompassLogo} className="w-12 h-12 mr-4" alt="Holidaze Logo" />
-          <span className="self-center text-xl font-semibold text-black whitespace-nowrap dark:text-white">Holidaze</span>
+          <span className="self-center text-xl font-semibold text-black whitespace-nowrap">Holidaze</span>
         </p>
       <Nav />
     </header>
